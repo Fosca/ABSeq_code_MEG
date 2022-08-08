b2261b7113fbbec0d57eaaa591be0d884b73326c
Fosca <fosca.alroumi@gmail.com>
2022-08-08 18:44:14 +0200

    corrected dependency

--- run_cluster.py
+++ run_cluster.py
@@ -3,7 +3,7 @@ from functions import cluster_funcs
 
 print('--- --- --- send on the cluster --- --- --- ')
 
-cluster_funcs.create_qsub('convert_data_to_BIDS', 'BIDS', 'BIDS', queue='Nspin_run32')
+cluster_funcs.create_qsub('convert_data_to_BIDS', 'BIDS', 'BIDS', queue='NSpin_long')
 
 print('--- --- --- finished sending on the cluster --- --- --- ')
 
